71b4b97eb4d4c1b870362d9acebf58db6cc09bd8
ColemanM0416 <colemanm0416@student.faytechcc.edu>
2026-09-12 13:43:37 -0400

    Add files via upload
    python hw1

---
+++ P1HW1_ColemanMagan.py
@@ -0,0 +1,29 @@
+# Magan Coleman
+# 09/11/2026
+# P1HW1
+# Calculating exponents amd addition and subrtaction
+
+
+# calculate exponents
+
+print("--------Exponents---------")
+print()
+
+base = int(input("Enter a base number: "))
+exponent = int(input("Enter an exponent: "))
+result = base ** exponent
+print(base, "raised to the power of", exponent, "is", result, "!!")
+
+# calculate addition and subtraction
+print("--------Addition and Subtraction---------")
+print()
+
+num1 =int(input("Enter a starting integer:  "))
+num2 =int(input("Enter an integer to add:  "))
+num3 =int(input("Enter an integer to subtract:  "))
+
+sum_result = num1 + num2
+final_result = sum_result - num3
+
+print(num1, "+", num2, "-", num3, "is equal to ", final_result, "!!")
+
